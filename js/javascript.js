e907c6be6c5a0bfb9577d10fb75081d15660b84d
Will Sutherland <wgpsutherland@gmail.com>
2014-06-10 18:15:32 +0100

    Shows like counts
    - like count displays
    - overlaid on image
    - username as opposed to just band name

--- js/javascript.js
+++ js/javascript.js
@@ -7,30 +7,24 @@ $(function(){
         url: "https://api.instagram.com/v1/users/43153969/media/recent/?access_token=222716143.cde9b68.04e77477b2fd4524a0d2a91fb170142f",
         success: function(data) {
 
-        	var count = 0;
-
 	        for (var i = 0; i < 20; i++) {
 
 				$("#instafeed").append("<div class='instaframe' id='image"+i+"'><img class='instaimage' src='" + data.data[i].images.standard_resolution.url +"'/></div>");  
 
 				document.getElementById("image"+i+"").style.width = "25%";
 
+				$("#image"+i+"").append("<p id='likeCount'>"+data.data[i].likes.count+"</p>");
 
-
-
-				/*document.getElementById("image"+i+"").style.width = "25%"; 
-
-				if (count==4 || count==5) {
-					document.getElementById("image"+i+"").style.width = "50%"; 
-				}
-
-				count++;
-
-				if (count==6) {
-					count=0;
-				}*/
 	      	}
+
 	      	document.getElementById("image0").style.width = "50%";
+	      	//document.getElementById("image0").style.opacity = "0.5";
+	      	//document.getElementById("likeCount").style.position = "absolute";
+	      	//document.getElementById("likeCount").style.zindex = "10";
+	      	//document.getElementById("likeCount").style.top = "0";
+	      	//document.getElementById("likeCount").style.bottom = "0";
+	      	//document.getElementById("likeCount").style.left = "0";
+	      	//document.getElementById("likeCount").style.right = "0";
         }
     });
 });
