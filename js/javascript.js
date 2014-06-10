20e1381cc88d8f59258a9f18c756a088e52d3aae
Will Sutherland <wgpsutherland@gmail.com>
2014-06-10 19:17:17 +0100

    Error checking

--- js/javascript.js
+++ js/javascript.js
@@ -4,7 +4,7 @@ $(function(){
     	type: "GET",
         dataType: "jsonp",
         cache: false,
-        url: "https://api.instagram.com/v1/users/43153969/media/recent/?client_id=cde9b68da7084efb88cec85619580eb0",
+        url: "https://api.instagram.com/v1/users/43153969/media/recent/??access_token=222716143.cde9b68.04e77477b2fd4524a0d2a91fb170142f",
         success: function(data) {
 
 	        for (var i = 0; i < 20; i++) {
@@ -19,6 +19,12 @@ $(function(){
 
 	      	document.getElementById("image0").style.width = "50%";
 	      	//document.getElementById("image0").style.opacity = "0.5";
+	      	//document.getElementById("likeCount").style.position = "absolute";
+	      	//document.getElementById("likeCount").style.zindex = "10";
+	      	//document.getElementById("likeCount").style.top = "0";
+	      	//document.getElementById("likeCount").style.bottom = "0";
+	      	//document.getElementById("likeCount").style.left = "0";
+	      	//document.getElementById("likeCount").style.right = "0";
         }
     });
 });
